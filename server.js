1ddff2e188d265a90c9c8b93794d8fdc64565e34
melmare <48948850+melmare@users.noreply.github.com>
2019-05-03 14:49:56 +0200

    create backend logic

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const uid = require('uid');
+const app = express();
+const fs = require('fs');
+app.use(express.json());
+app.use(express.static('./dist'));
+
+const filePath = __dirname + '/cards.json';
+let cards;
+
+readCards();
+
+app.listen(3000, err => {
+  err ? console.log(err) : console.log('Server is ready');
+});
+
+app.post('/cards', (req, res) => {
+  readCards();
+  const newCard = { ...req.body, id: uid() };
+  res.json(newCard);
+  cards = [...cards, newCard];
+  saveCards(cards);
+});
+
+app.get('/cards', (req, res) => {
+  res.json(cards);
+});
+
+app.get('/cards/:id', (req, res) => {
+  const { id } = req.params;
+  res.json(cards.find(card => card.id === id));
+});
+
+function readCards() {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      writeCardsFile();
+      console.log('cardsFile would be created');
+    } else {
+      cards = JSON.parse(data);
+    }
+  });
+}
+
+function saveCards(cards) {
+  fs.writeFile(filePath, JSON.stringify(cards), 'utf8', err => {
+    if (err) {
+      console.log('could not save new cards');
+    }
+  });
+}
+
+function writeCardsFile() {
+  fs.writeFile(filePath, 'utf8', err => {
+    if (err) {
+      console.log('could not write CardsFile');
+    }
+  });
+}
